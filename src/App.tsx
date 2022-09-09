a542c169dd345016e53ca168ebb04a9639f2761f
mohamadreza hedayat <mohamadrezahedayat@gmail.com>
2022-09-10 01:42:52 +0430

    react-refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './styles.css'
 import Image from './react-logo.png'
 import Logo from './react-logo.svg'
+import { ClickCounter } from './clickCounter'
 
 export const App = ()=>{
   return <>
     <h1>React Typescript Starter Template - running in {process.env.NODE_ENV} mode</h1>
     <img src={Image} alt='react-logo' width='300' height='300'/>
     <img src={Logo} alt='react-logo' width='300' />
+    <ClickCounter/>
     </>
 }
